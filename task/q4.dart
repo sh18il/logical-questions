ee3f2a20df633c83e69d38b1d3b545663525563c
muhammed shibil.m <sshibil379@gmail.com>
2024-08-02 22:57:38 +0530

    frist commit

---
+++ task/q4.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 100;
+  for (var i = 1; i <= a; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("FizzBuzz");
+    } else if (i % 3 == 0) {
+      print("Fizz");
+    } else if (i % 5 == 0) {
+      print("Buzz");
+    } else {
+      print(i);
+    }
+  }
+}
